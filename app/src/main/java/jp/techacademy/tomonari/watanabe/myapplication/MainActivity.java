8f5c6add7e07d287a4ed6929bdbe752113535564
tomonari-watanabe <kenapa_keppa@yahoo.co.jp>
2016-08-11 11:22:48 +0900

    仮完成

--- app/src/main/java/jp/techacademy/tomonari/watanabe/myapplication/MainActivity.java
+++ app/src/main/java/jp/techacademy/tomonari/watanabe/myapplication/MainActivity.java
@@ -1,13 +1,86 @@
 package jp.techacademy.tomonari.watanabe.myapplication;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView mTextView;
+
+    int sethour;
+    int setminute;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.TextView);
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.button1) {
+            showhello();
+        } else if (v.getId() == R.id.button2) {
+            settime();
+        }
+
+    }
+
+    private void settime() {
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        sethour = hourOfDay;
+                        setminute = minute;
+
+                        Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                    }
+                },
+                8, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+
+        timePickerDialog.show();
+
+    }
+
+    private void showhello(){
+
+        if (sethour >=2 && sethour <10 ){
+            mTextView.setText("おはよう");
+        } else if (sethour >=10 && sethour < 18){
+            mTextView.setText("こんにちは");
+        } else {
+            mTextView.setText("こんばんは");
+        }
+
+
+
+
+    }
+
+
 }
+
